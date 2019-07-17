be4db776433ef0b86b5062ca1892acdbd2d72c88
DoctorOneli <1006629349@qq.com>
2019-07-17 14:43:35 +0800

    增加取消订单，用户删除订单，修改订单地址，修改订单状态

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -47,4 +47,63 @@ class Index extends Controller
         $model = new ViewMyAddress();
         return json($model->where('user_id', '1')->select());
     }
+
+    /**
+     * 用户取消订单
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function orderCancel(Request $request)
+    {
+        $order_id = $request->param()['order_id'];  //订单id
+
+        $order = new Order();
+        $order = $order->where('order_id', $order_id)->find();
+
+        if (empty($order))
+
+            return json([
+                'code'  => '401',
+                'msg'   => '订单不存在'
+            ]);
+
+        $order->order_state_id = 5; //订单状态id改成取消订单id
+
+        $result = $order->save();
+
+
+        if (!$result) {
+            return json([
+                'code'  => 500,
+                'msg'   => 'update failed'
+            ]);
+        }
+
+        $orderItem = new OrderItem();
+        $orderList = $orderItem->where('order_id', $order_id)->select();
+
+        foreach ($orderList as $item) {
+            $book = new Book();
+            $book = $book->where('book_id', $item['book_id'])->find();
+
+            $count = $book->book_count + $item['count'];    //加回订单的数量
+
+            $book->book_count = $count;     //更改库存数量
+
+            $result = $book->save();
+
+            if (!$result) {
+                return json([
+                    'code'  => 500,
+                    'msg'   => 'update failed'
+                ]);
+            }
+        }
+
+        return json([
+            'code'  => 200,
+            'msg'   => '取消成功'
+        ]);
+    }
 }
